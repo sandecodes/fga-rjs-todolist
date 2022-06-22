610eb10c5c94a0b505b38f91f4bf25cfc6d68018
sandecodes <muhammadsandy74@gmail.com>
2022-06-22 17:06:49 +0700

    add credit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="header">
-      <h1>Todo List</h1>
+    <div>
+      <div className="header">
+        <h1>Todo List</h1>
+      </div>
+      <div className="credit">
+        Create With ❤️{" "}
+        <a href="https://www.github.com/sandecodes" target="_blank" rel="noopener noreferrer">
+          sandecodes
+        </a>
+      </div>
     </div>
   );
 };
